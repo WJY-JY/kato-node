e553b3d6cc1c6a584d8ba04f03b21f2adaa118b4
王加雨 <wjy.jy@foxmail.com>
2019-03-05 10:41:31 +0800

    JSON解析日期正则表达添加开始结束符

--- dist/index.js
+++ dist/index.js
@@ -145,7 +145,7 @@ exports.KatoRuntimeError = KatoRuntimeError;
 
 Object.defineProperty(exports, "__esModule", { value: true });
 const dayjs = __webpack_require__(23);
-const ISO8601Regex = /(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+([+-][0-2]?\d:[0-5]\d|Z))|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d([+-][0-2]?\d:[0-5]\d|Z))/;
+const ISO8601Regex = /^(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+([+-][0-2]?\d:[0-5]\d|Z))|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d([+-][0-2]?\d:[0-5]\d|Z))$/;
 function reviver(key, value) {
     if (typeof value === 'string')
         if (ISO8601Regex.exec(value))
